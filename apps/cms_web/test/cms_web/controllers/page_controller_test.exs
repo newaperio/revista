1d4a612f0a744bbbcaddd47c2a57094ccda0c4e5
Nicholas Scheurich <nick@scheurich.me>
2018-10-31 15:48:09 -0500

    Make test green

--- apps/cms_web/test/cms_web/controllers/page_controller_test.exs
+++ apps/cms_web/test/cms_web/controllers/page_controller_test.exs
@@ -1,8 +1,12 @@
 defmodule CMSWeb.PageControllerTest do
   use CMSWeb.ConnCase
 
+  setup do
+    Ecto.Adapters.SQL.Sandbox.checkout(CMS.Repo)
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Posts"
   end
 end
--- apps/cms_web/test/test_helper.exs
+++ apps/cms_web/test/test_helper.exs
@@ -1,1 +1,3 @@
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(CMS.Repo, :manual)
