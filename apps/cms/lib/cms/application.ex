382be5e3338470cea34df8a9935b1be4c1409c23
Nicholas Scheurich <nick@scheurich.me>
2018-10-31 15:47:17 -0500

    Add comments

--- apps/cms/lib/cms/application.ex
+++ apps/cms/lib/cms/application.ex
@@ -1,13 +1,18 @@
 defmodule CMS.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
+    # List all child processes to be supervised
     children = [
       {CMS.Repo, []}
     ]
 
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CMS.Supervisor]
     Supervisor.start_link(children, opts)
   end
