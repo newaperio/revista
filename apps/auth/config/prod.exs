54140be2fdf99aeda94098a5b210277140642fb5
Nicholas Scheurich <nick@scheurich.me>
2018-11-01 18:54:52 -0500

    Fix syntax errors

--- apps/auth/config/prod.exs
+++ apps/auth/config/prod.exs
@@ -6,4 +6,4 @@ config :auth, Auth.Repo,
   database: "auth_prod",
   url: System.get_env("DATABASE_URL"),
   ssl: true,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
--- apps/cms/config/prod.exs
+++ apps/cms/config/prod.exs
@@ -6,4 +6,4 @@ config :cms, CMS.Repo,
   database: "cms_prod",
   url: System.get_env("DATABASE_URL"),
   ssl: true,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
