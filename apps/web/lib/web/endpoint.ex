6e858161293b51b52254143598debce0e9206663
Nicholas Scheurich <nick@scheurich.me>
2018-11-01 14:23:59 -0500

    Slim down app

--- apps/web/lib/web/endpoint.ex
+++ apps/web/lib/web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :web
 
-  socket "/socket", Web.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
