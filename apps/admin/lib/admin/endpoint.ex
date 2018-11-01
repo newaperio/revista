6db3a78ec1104403e2a8c30dd1967cbc3c108ec5
Nicholas Scheurich <nick@scheurich.me>
2018-11-01 14:27:50 -0500

    Slim down app

--- apps/admin/lib/admin/endpoint.ex
+++ apps/admin/lib/admin/endpoint.ex
@@ -1,11 +1,6 @@
 defmodule Admin.Endpoint do
   use Phoenix.Endpoint, otp_app: :admin
 
-  socket("/socket", Admin.UserSocket,
-    websocket: true,
-    longpoll: false
-  )
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
