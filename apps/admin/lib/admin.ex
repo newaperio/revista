443611a418ffd32dd498342610b19cacf67b1695
Nicholas Scheurich <nick@scheurich.me>
2018-10-31 19:45:43 -0500

    Improve docs

--- apps/admin/lib/admin.ex
+++ apps/admin/lib/admin.ex
@@ -1,7 +1,10 @@
 defmodule Admin do
   @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
+  The admin system for the platform.
+
+  This is entrypoint for defining the web interface to the
+  content management system, such as controllers, views,
+  channels and so on.
 
   This can be used in your application as:
 
@@ -9,12 +12,12 @@ defmodule Admin do
       use Admin, :view
 
   The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
+  controller, etc, so keep them short and clean, focused on
+  imports, uses and aliases.
 
   Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
+  below. Instead, define any helper function in modules and
+  import those modules here.
   """
 
   def controller do
