c1232a8a5afb7720b0a21c1f9db527f37a5ad08f
Nicholas Scheurich <nick@scheurich.me>
2018-11-05 18:19:05 -0600

    Move runtime configuration into a Distillery config provider

--- apps/cms/config/prod.exs
+++ apps/cms/config/prod.exs
@@ -2,9 +2,3 @@ use Mix.Config
 
 config :logger, level: :info
 
-config :cms, CMS.Repo,
-  database: "cms_prod",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  pool_size: 10
