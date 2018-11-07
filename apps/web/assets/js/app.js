d4aefda82bb9545240edf9e1c132a949270be02f
Nicholas Scheurich <nick@scheurich.me>
2018-11-07 12:39:38 -0600

    Update WebSockets URL

--- apps/web/assets/js/app.js
+++ apps/web/assets/js/app.js
@@ -1,25 +1,25 @@
-import { Socket } from "phoenix";
-import React from "react";
-import ReactDOM from "react-dom";
-import TweetList from "./components/TweetList";
-import "phoenix_html";
-import "../css/app.scss";
+import {Socket} from 'phoenix';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TweetList from './components/TweetList';
+import 'phoenix_html';
+import '../css/app.scss';
 
-const SOCKET_URL = "ws://localhost:4002/socket";
+const SOCKET_URL = 'ws://twitter.elixirinthe.cloud/socket';
 
 const socket = new Socket(SOCKET_URL, {
-  params: { token: window.userToken }
+  params: {token: window.userToken},
 });
 
 socket.connect();
 
-const channel = socket.channel("tweets", {});
+const channel = socket.channel('tweets', {});
 channel
   .join()
-  .receive("ok", resp => console.log("Joined Tweets channel", resp))
-  .receive("error", resp => console.log("Unable to join Tweets channel", resp));
+  .receive('ok', resp => console.log('Joined Tweets channel', resp))
+  .receive('error', resp => console.log('Unable to join Tweets channel', resp));
 
-document.addEventListener("DOMContentLoaded", () => {
-  const node = document.getElementById("tweets");
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('tweets');
   ReactDOM.render(<TweetList channel={channel} />, node);
 });
